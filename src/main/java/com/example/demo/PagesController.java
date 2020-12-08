5c2feb251a5202b11e6f67be65ef1dc9576441c2
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-08 14:59:56 -0800

    added comment posting example

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class PagesController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello world";
+    }
+
+    @PostMapping("/comments")
+    public String postComment(String author, String content) {
+        return String.format("%s said %s!", author, content);
     }
 }
--- src/test/java/com/example/demo/PagesControllerTest.java
+++ src/test/java/com/example/demo/PagesControllerTest.java
@@ -3,10 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Random;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,5 +27,19 @@ public class PagesControllerTest {
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello world"));
+    }
+
+    @Test
+    public void testCreateComment() throws Exception {
+        String content = String.valueOf(new Random().nextInt());
+
+        MockHttpServletRequestBuilder request = post("/comments")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("content", content)
+                .param("author", "Dwayne");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.format("Dwayne said %s!", content)));
     }
 }

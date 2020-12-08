f949ba573eb149b87870c87d400e1892d11d4b69
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-08 11:22:29 -0800

    returned pi with spring

---
+++ src/test/java/com/galvanize/springMath/SpringMathPiWithGetTest.java
@@ -0,0 +1,26 @@
+package com.galvanize.springMath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SpringMathPiWithGet.class)
+public class SpringMathPiWithGetTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testGetPi() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/pi");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("3.141592653589793"));
+    }
+}

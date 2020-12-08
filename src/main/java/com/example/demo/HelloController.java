c9b6cc9beca68560240ac137293970871ebd1576
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-08 11:21:56 -0800

    did more learn stuff

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/app")
 public class HelloController {
-
     @GetMapping("/")
     public String helloWorld() {
         return "Hello from Spring!";
+    }
+
+    @GetMapping("/tasks")
+    public String getTasks() {
+        return "These are tasks";
+    }
+
+    @PostMapping("/tasks")
+    public String createTask() {
+        return "You just POSTed to /tasks";
     }
 
 }

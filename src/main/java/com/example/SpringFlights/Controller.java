07ca53e0d980bd4d23458a50cf1034e516656239
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-09 13:11:40 -0800

    get all flights, even null values

--- src/main/java/com/example/SpringFlights/Controller.java
+++ src/main/java/com/example/SpringFlights/Controller.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/flights")
 public class Controller {
@@ -13,5 +16,19 @@ public class Controller {
         flight.setDeparts(2017, 4, 21, 14, 34);
         flight.addTicket(200, "Some name", "Some other name");
         return flight;
+    }
+
+    @GetMapping("")
+    public ArrayList<Flight> getAllFlights() {
+        ArrayList<Flight> flights = new ArrayList<>();
+        Flight flight01 = new Flight();
+        flight01.setDeparts(2017, 4, 21, 14, 34);
+        flight01.addTicket(200, "Some name", "Some other name");
+        flights.add(flight01);
+        Flight flight02 = new Flight();
+        flight02.setDeparts(2017, 4, 21, 14, 34);
+        flight02.addTicket(400, "Some other name", null);
+        flights.add(flight02);
+        return flights;
     }
 }
--- src/test/java/com/example/SpringFlights/ControllerTest.java
+++ src/test/java/com/example/SpringFlights/ControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,5 +37,25 @@ public class ControllerTest {
                 .andExpect(jsonPath("$.tickets[0].passenger.firstName", is("Some name")))
                 .andExpect(jsonPath("$.tickets[0].passenger.lastName", is("Some other name")))
                 .andExpect(jsonPath("$.tickets[0].price", is(200)));
+    }
+
+    @Test
+    public void getAllFlights() throws Exception {
+        String flight01 = "{\"departs\":\"2017-04-21 14:34\",\"tickets\":[{\"passenger\":{\"firstName\":\"Some name\",\"lastName\":\"Some other name\"},\"price\":200}]}";
+        String flight02 = "{\"departs\":\"2017-04-21 14:34\",\"tickets\":[{\"passenger\":{\"firstName\":\"Some other name\",\"lastName\":null},\"price\":400}]}";
+
+        this.mvc.perform(
+                get("/flights")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].departs", is("2017-04-21 14:34")))
+                .andExpect(jsonPath("$[0].tickets[0].passenger.firstName", is("Some name")))
+                .andExpect(jsonPath("$[0].tickets[0].passenger.lastName", is("Some other name")))
+                .andExpect(jsonPath("$[0].tickets[0].price", is(200)))
+                .andExpect(jsonPath("$[1].departs", is("2017-04-21 14:34")))
+                .andExpect(jsonPath("$[1].tickets[0].passenger.firstName", is("Some other name")))
+                .andExpect(jsonPath("$[1].tickets[0].passenger.lastName", is(nullValue())))
+                .andExpect(jsonPath("$[1].tickets[0].price", is(400)));
     }
 }

cb44bc036a802626303f1219d39b5f12e8581dbf
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-11 12:41:10 -0800

    added custom get query, by title

--- src/main/java/com/example/crud/LessonRepository.java
+++ src/main/java/com/example/crud/LessonRepository.java
@@ -3,4 +3,5 @@ package com.example.crud;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LessonRepository extends CrudRepository<Lesson, Long> {
+    Lesson findByTitle(String title);
 }

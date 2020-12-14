4d64f3e13e4e82b36397564947ba750a6f1b1f3c
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-14 13:08:11 -0800

    fit the test to match the output, ain't nobody got time for gson'ing

--- src/test/java/com/example/SpringWord/WordCounterControllerTest.java
+++ src/test/java/com/example/SpringWord/WordCounterControllerTest.java
@@ -3,30 +3,22 @@ package com.example.SpringWord;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WordCounterControllerTest {
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Test
-//    void getWordCount() throws Exception {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getWordCount() throws Exception {
 //        String expectedJSON =
 //                "{\n" +
 //                "  \"A\": 1,\n" +
@@ -37,16 +29,18 @@ class WordCounterControllerTest {
 //                "  \"a\": 1,\n" +
 //                "  \"fox\": 1\n" +
 //                "}\n";
-//
-//        String inputText = "A brown cow jumps over a brown fox";
-//
-//        MockHttpServletRequestBuilder request = post("/words/count")
-//                .accept(MediaType.APPLICATION_JSON)
-//                .contentType(MediaType.TEXT_PLAIN)
-//                .content(inputText);
-//
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", is(expectedJSON)));
-//    }
+//        expectedJSON = expectedJSON.replaceAll("\n", "");
+        String expectedJSON = "{\"over\":1,\"A\":1,\"a\":1,\"jumps\":1,\"cow\":1,\"brown\":2,\"fox\":1}";
+
+        String inputText = "A brown cow jumps over a brown fox";
+
+        MockHttpServletRequestBuilder request = post("/words/count")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.TEXT_PLAIN)
+                .content(inputText);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedJSON));
+    }
 }
